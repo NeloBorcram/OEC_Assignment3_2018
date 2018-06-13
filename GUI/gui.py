8901484ac454964a03f6b133edb5904f06ce3660
viktorPlue <viktor.plue@gmail.com>
2018-06-13 17:40:56 +0000

    gui comments updated

--- GUI/gui.py
+++ GUI/gui.py
@@ -24,36 +24,58 @@ class MainWindow(object):
 
         Returns: GUI window
         """
+        
+        """
+        main_window wird deklariert, größe wird auf 500x550 gesetzt und Überschrift für Fenster wird gesetzt.
+        """
         main_window = Tk()
         main_window.title("Assignment3")
         main_window.geometry("500x550")
+        """
+        Label(ein Textfeld) wird angelegt und dem main_window zugewiesen,
+        
+        durch pack() wird ein Container ähnlich wie bei einem StackPanel erstellt, der die einzelnen Elemente grupiert!
+
+        """
         group = Label(main_window, text="Jahrgang: BWI16-BB",)
         group.pack()
+        """Hier wird analog zu oben wieder ein Textfeld erstellt."""
+        
         description = Label(main_window, text="Description: Each device(Raspberry Pi) in the Local Network will be listed here!\n"
                                           "If a device (Pi) entry or loss the list will be changed!\n"
                                           "There is also a master election under the devices(Pis)!\n",)
         description.pack()
+        
+        """
+        Hier wird self.master als variable Klasse instanziert(self.master = StringVar(), weiters wieder wie oben beschrieben dem Fenster hinzugefügt)
+        """
         self.master = StringVar()
         Label(main_window, textvariable=self.master).pack()
         self.master.set("Master: ")
 
+
+        """Das selbe wie oben"""
         self.own_ip = StringVar()
         Label(main_window, textvariable=self.own_ip).pack()
         self.own_ip.set("Own IP: ")
 
+        """List box wird instanziert & größe festgelegt!"""
         self.ip_address_list_box = Listbox(main_window, width=70, height=20)
         self.ip_address_list_box.pack()
 
+        """Start Button wird instanziert, diesem wird ein Text zugewiesen, und mit einer Methode verbunden"""
         self.startBtn = Button(main_window, text="Start Com", command=self.com_start_callback)
         self.startBtn.pack()
 
+        """selbes wie obriger button"""
         self.stopBtn = Button(main_window, text="Stop Com", command=self.com_stop_callback)
         self.stopBtn.pack()
 
+        """Keine Ahnung was da passiert"""
         self.state = StringVar()
         Label(main_window, textvariable=self.state).pack()
         main_window.protocol("WM_DELETE_WINDOW", self._on_closing)
-
+        "Die Instanz des Main Windows wird zurück gegeben"
         return main_window
 
     def show(self):
